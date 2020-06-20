8a3f7f5963ca0f7911aa9ba9720bfa64d069b98a
Riina K <riinak.tv@gmail.com>
2020-06-20 11:39:00 +0900

    Implement changing behavior of automatic posting .

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ class Behavior {
     this.interval(true);
   }
 
+  channelName() {
+    return 'bot_test';
+  }
+
   interval(boot) {
     if (! boot) {
       let content = moment().format('YYYY-MM-DD HH:mm:ss');
@@ -33,20 +37,70 @@ class Behavior {
     this.timeoutId = setTimeout(() => { this.interval(false) }, this.intervalNextSeconds * 1000);
   }
 
-  channelName() {
-    return 'bot_test';
-  }
+  nextInterval() {
+    let next = this.prevIntervalTime.add(this.intervalNextSeconds, 'seconds');
+    return next.format('YYYY-MM-DD HH:mm:ss');
+  } // function nextInterval()
+
+  // bot command
+  command(command) {
+    let match;
+    if (match = command.match(/^interval(\s|$)/)) {
+
+      // interval command
+      if (match = command.match(/^interval\s+next/)) {
+        // show next interval time
+        let next = this.nextInterval();
+        this.channel.send(`bot mode : next interval is ${next}`);
+      } else if (match = command.match(/^interval\s+(?<interval>\d+)/)) {
+        // change interval time
+        let intervalMinutes = match.groups.interval;
+        this.intervalPerMinutes = parseInt(intervalMinutes);
+        this.interval(true);
+        sender.send(this.channel, `bot mode : set interval to ${this.intervalPerMinutes} minutes`);
+        console.log(`bot interval changed : ${this.intervalPerMinutes} minutes`);
+      } else {
+        // show current interval
+        sender.send(this.channel, `bot mode : current interval is ${this.intervalPerMinutes} minutes`);
+      }
+
+    } else {
+      this.help()
+    }
+  } // command()
+
+  help() {
+    let content = `bot command\n`
+      + `bot interval : Display notification interval for current time.\n`
+      + `bot interval (5|10|15|20|30|60) : Change the notification for the current time, specify the desired minutes.`
+      + `bot interval next : Show next time for the notification.`;
+    sender.send(this.channel, content);
+  } // function help()
 
   message(message) {
     let content = message.content;
     let channel = message.channel;
     let author = message.author.username;
-    sender.send(message, content);
+    sender.reply(message, content);
   }
 
   mention(message) {
-    let content = 'Hi, there.';
-    sender.send(message, content);
+    let content = message.content;
+    let match = content.match(/^<@!?\d+>\s+(?<body>.*)$/);
+    let body = match.groups.body;
+
+    if (body.match(/^bot(\s|$)/)) {
+      if (match = body.match(/^bot\s+?(?<command>.*?)$/)) {
+        let command = match.groups.command;
+        this.command(command);
+        console.log(`command : ${command}`);
+      } else {
+        this.help();
+      }
+    } else {
+      let content = 'Hi, there.';
+      sender.reply(message, content);
+    }
   }
 }
 
