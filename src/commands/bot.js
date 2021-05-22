bddbf089fbf875684a1392ec376b8b9e06de59a9
Riina K <riinak.tv@gmail.com>
2021-05-22 19:53:28 +0900

    behave well the bot command

--- src/commands/bot.js
+++ src/commands/bot.js
@@ -9,41 +9,45 @@ module.exports = {
   execute (message, args) {
     const b = message.client.behavior
     if (args.length) {
-      const subcommand = args.shift().toLowerCase()
-      if (subcommand === 'interval') {
-        if (args.length) {
-          const amount = parseInt(args.shift())
-          if (!isNaN(amount)) {
-            // change interval time
-            b.intervalPerMinutes = amount
-            b.interval(true)
-            sender.send(
-              b.channel,
-              `bot mode : set interval to ${b.intervalPerMinutes} minutes`
-            )
-            log.info(`bot interval changed : ${b.intervalPerMinutes} minutes`)
-            return
+      const command = args.shift().toLowerCase()
+      switch (command) {
+        case 'interval': {
+          if (args.length) {
+            const amount = parseInt(args.shift())
+            if (!isNaN(amount)) {
+              // change interval time
+              b.intervalPerMinutes = amount
+              b.interval(true)
+              sender.send(
+                b.channel,
+                `bot mode : set interval to ${b.intervalPerMinutes} minutes`
+              )
+              log.info(`bot interval changed : ${b.intervalPerMinutes} minutes`)
+              return
+            }
           }
+          // show current interval
+          const next = b.nextInterval()
+          const content = [
+            '**bot interval** :',
+            `  current interval is \`${b.intervalPerMinutes} minutes\``,
+            `  next interval is \`${next}\``
+          ]
+          sender.send(b.channel, content)
+          return
         }
-        // show current interval
-        const next = b.nextInterval()
-        sender.send(
-          b.channel,
-          '**bot mode** :\n' +
-          `  current interval is \`${b.intervalPerMinutes} minutes\`\n` +
-          `  next interval is \`${next}\``
-        )
-        return
-      }
-    }
+      } // switch
+    } // if (args.length)
 
     // help
-    const content = '**bot command**\n' +
-      '`bot interval` : Display automatic notification interval as minutes.\n' +
-      '  `bot interval (5|10|15|20|30|60)` : \n' +
-      '    Change the automatic notification intercal, specify the minutes.\n' +
-      '  `bot interval next` : \n' +
+    const content = [
+      '**bot command**',
+      '`bot interval` : Display automatic notification interval as minutes.',
+      '  `bot interval (5|10|15|20|30|60)` : ',
+      '    Change the automatic notification intercal, specify the minutes.',
+      '  `bot interval next` : ',
       '    Show next time for the automatic notification.'
+    ]
     sender.send(b.channel, content)
   }
 }
