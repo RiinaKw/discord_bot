f65a64e61de40eec511bb41093cf02daa0133150
Riina K <riinak.tv@gmail.com>
2021-05-23 00:47:33 +0900

    add script to register a slash command

---
+++ src/register-command.js
@@ -0,0 +1,72 @@
+'use strict'
+
+console.log('ここ読め : https://discord.com/developers/docs/interactions/slash-commands')
+
+const request = require('request')
+
+/*
+const json = {
+  name: 'blep',
+  description: 'Send a random adorable animal photo',
+  options: [
+    {
+      name: 'animal',
+      description: 'The type of animal',
+      type: 3,
+      required: true,
+      choices: [
+        {
+          name: 'Dog',
+          value: 'animal_dog'
+        },
+        {
+          name: 'Cat',
+          value: 'animal_cat'
+        },
+        {
+          name: 'Penguin',
+          value: 'animal_penguin'
+        }
+      ]
+    },
+    {
+      name: 'only_smol',
+      description: 'Whether to show only baby animals',
+      type: 5,
+      required: false
+    }
+  ]
+}
+*/
+const json = {
+  name: 'bot',
+  description: 'Execute a bot command',
+  options: [
+    {
+      name: 'subcommand',
+      description: 'sub command',
+      type: 5,
+      required: false
+    }
+  ]
+}
+
+const url = 'https://discord.com/api/v8/applications/720987257733120080/commands'
+const token = require('../config/token')
+
+const options = {
+  uri: url,
+  headers: {
+    'Content-type': 'application/json',
+    Authorization: 'Bot ' + token
+  },
+  json: json
+}
+
+request.post(options, function (err, res, body) {
+  if (err) {
+    console.log('Error: ' + err.message)
+    return
+  }
+  console.log(body)
+})
